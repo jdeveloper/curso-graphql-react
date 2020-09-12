5f377b29c6a315eaf7473ed42dbc7bf081f0736a
jota <jdeveloper.inxenio@gmail.com>
2020-09-12 11:04:04 +0200

    Added user authentication

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,10 +1,23 @@
 const Usuario = require('../model/Usuario');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: 'variables.env'});
+
+const crearToken = (usuario, secret, expiresIn) => {
+    console.log(usuario);
+    const { id, email, nombre, apellido } = usuario;
+
+    return jwt.sign({ id, email, nombre, apellido }, secret, { expiresIn });
+}
 
 // resolvers
 const resolvers = {
     Query : {
-        obtenerCurso: () => "Algo"
+        obtenerUsuario: async (_, {token}) => {
+            const usuarioId = await jwt.verify(token, process.env.SECRET);
+
+            return usuarioId;
+        }
     },
     Mutation: {
         nuevoUsuario: async (_, { input }) => {
@@ -27,6 +40,25 @@ const resolvers = {
             }
 
             return 'Creando...';
+        },
+
+        autenticarUsuario: async (_, { input }) => {
+            const { email, password } = input;
+            const existeUsuario = await Usuario.findOne({email});
+
+            if (!existeUsuario) {
+                throw new Error('El usuario no existe');
+            }
+
+            const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+
+            if (!passwordCorrecto) {
+                throw new Error('El password no es correcto');
+            }
+
+            return {
+                token: crearToken(existeUsuario, process.env.SECRET, '24h')
+            };
         }
     }
 };
--- db/schema.js
+++ db/schema.js
@@ -10,6 +10,10 @@ const typeDefs = gql`
         creado: String
     }
 
+    type Token {
+        token: String
+    }
+
     input UsuarioInput {
         nombre: String!
         apellido: String!
@@ -17,12 +21,18 @@ const typeDefs = gql`
         password: String!
     }
 
+    input AutenticarInput {
+        email: String!
+        password: String!
+    }
+
     type Query {
-        obtenerCurso: String
+        obtenerUsuario(token: String!): Usuario
     }
 
     type Mutation {
         nuevoUsuario(input: UsuarioInput): Usuario
+        autenticarUsuario(input: AutenticarInput) : Token
     }
 `;
 
